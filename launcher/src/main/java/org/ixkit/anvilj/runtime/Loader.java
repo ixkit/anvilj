1d5638bbee514c1c034fc674ef80b03c96eb416c
icoco <tfp10@msn.com>
2023-12-01 21:39:30 +0800

    integrate with addons project which is outside of the sketch project

---
+++ launcher/src/main/java/org/ixkit/anvilj/runtime/Loader.java
@@ -0,0 +1,10 @@
+package org.ixkit.anvilj.runtime;
+
+import org.ixkit.things.equipment.controller.EquipmentController;
+
+public class Loader {
+
+    private EquipmentController controller;
+
+    private org.ixkit.things.twinscenario.controller.TwinScenarioController scenarioController;
+}
