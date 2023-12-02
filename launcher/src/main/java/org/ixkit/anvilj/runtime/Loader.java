f30192fb54386ad6ed6c2830f2ca53076b72cfaf
icoco <tfp10@msn.com>
2023-12-02 21:14:06 +0800

    idea ready

--- launcher/src/main/java/org/ixkit/anvilj/runtime/Loader.java
+++ launcher/src/main/java/org/ixkit/anvilj/runtime/Loader.java
@@ -1,10 +1,16 @@
 package org.ixkit.anvilj.runtime;
 
+
 import org.ixkit.things.equipment.controller.EquipmentController;
+import org.ixkit.things.twinscenario.controller.TwinScenarioController;
 
 public class Loader {
 
     private EquipmentController controller;
 
-    private org.ixkit.things.twinscenario.controller.TwinScenarioController scenarioController;
+    private TwinScenarioController scenarioController;
+
+    public Loader(EquipmentController controller) {
+        this.controller = controller;
+    }
 }
